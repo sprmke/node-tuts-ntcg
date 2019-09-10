e2952334df357661ee2426f79b7325e9fefbad55
Michael Manlulu <michael.manlulu@tooltwist.com>
2019-09-10 14:33:46 +0800

    095 Finishing the controllers

--- app.js
+++ app.js
@@ -10,15 +10,13 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorConroller = require('./controllers/error')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorConroller.get404);
 
 app.listen(3000);
