3d2c6cc25e17b3f83dd8d3904c8898e8968ce5f6
Michael Manlulu <michael.manlulu@tooltwist.com>
2019-09-11 13:59:09 +0800

    099 Refactoring the file storage

--- models/product.js
+++ models/product.js
@@ -1,24 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
+const productPath = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = callback => {
+  fs.readFile(productPath, (err, fileContent) => {
+    if (err) {
+      callback([]);
+    } else {
+      callback(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const productPath = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-
-    fs.readFile(productPath, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(productPath, JSON.stringify(products), err => {
         console.log(err)
@@ -27,18 +32,6 @@ module.exports = class Product {
   }
 
   static fetchAll(callback) {
-    const productPath = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-
-    fs.readFile(productPath, (err, fileContent) => {
-      if (err) {
-        callback([]);
-      } else {
-        callback(JSON.parse(fileContent));
-      }
-    });
+    getProductsFromFile(callback);
   }
 }
