3b9a0bb0573c49f719ba48536c3d4e800631caee
Michael Manlulu <michael.manlulu@tooltwist.com>
2019-09-18 22:28:49 +0800

    120 using query params

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,18 @@ const router = express.Router();
 
 const adminController = require('../controllers/admin');
 
-// /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
-
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+// /admin/edit-product/:productId => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+
 
 module.exports = router;
